32cbadadbac2f824b8a8713233fb9ab3e55e2c27
nvdinh185 <nvdinh0766777123@gmail.com>
2020-09-30 14:59:37 +0700

    connect to server

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const documents = {};
-
 io.on('connection', socket => {
-    console.log(`Socket ${socket.id} has connected`);
+    console.log(`Socket ${socket.id} has connected!!!`);
+    socket.on("disconnect", () => {
+        console.log(`Socket ${socket.id} disconnected!!!`);
+    });
 });
 
 http.listen(4444, () => {
