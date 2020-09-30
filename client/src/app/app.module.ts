4642c7e14f83866d397a6a253f933a12243805fb
nvdinh185 <nvdinh0766777123@gmail.com>
2020-09-30 10:04:37 +0700

    initial socket

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,16 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
-import { DocumentListComponent } from './components/document-list/document-list.component';
-import { DocumentComponent } from './components/document/document.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DocumentListComponent,
-    DocumentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
